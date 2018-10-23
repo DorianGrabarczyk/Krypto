e88b96f1a78e608bf509e6bea68f06128b75047b
DorianGrabarczyk <43935979+DorianGrabarczyk@users.noreply.github.com>
2018-10-23 18:38:51 +0200

    Rzowiazanie z mapą

---
+++ Wzaip/integerFactor.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+
+using namespace std;
+int main()
+{
+    long long liczba, i;
+	while (true)
+	{
+		cin >> liczba;
+		if(liczba == 0)
+            break;
+        map <long long, int> _map;
+        map <long long, int>::iterator _iterator;
+		for (i = 2; i*i <= liczba; i++)
+		{
+			while (liczba%i == 0)
+			{
+				_map[i]++;
+				liczba /= i;
+			}
+		}
+		if (liczba != 1) _map[liczba]++;
+		for(_iterator = _map.begin();_iterator!= _map.end();_iterator++)
+            cout << _iterator->first << "^" << _iterator->second << " ";
+		cout << "\n";
+	}
+	return 0;
+}
